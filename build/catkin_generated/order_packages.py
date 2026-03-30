696e864d6e07a9618f318fd26fd8e915408b7037
GucyOuO <zzg1027@gmail.com>
2026-03-30 12:46:25 +0800

    init ROS workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/zzg/demo05_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/zzg/demo05_ws/devel;/home/zzg/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/zzg/demo05_ws/devel;/home/zzg/catkin_ws/devel;/opt/ros/noetic' != '' else []
